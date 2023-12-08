098224a34e14616d14feddf78f180ff297784606
GusEngers <gustavoengers6441@gmail.com>
2023-12-08 01:08:21 -0300

    Quitar dependencias de testing - Cambiar npm por pnpm - Añadir fuente y dependencia para iconos

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Icon } from '@iconify/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola Mundo</h1>
+      <Icon icon={'fa6-solid:o'} width={'100'} />
+      <Icon icon={'fa6-solid:x'} width={'200'} />
     </div>
   );
 }
