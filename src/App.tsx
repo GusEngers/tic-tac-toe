1ba10c8b5ee201895672f4fc7f6290b98738227b
GusEngers <gustavoengers6441@gmail.com>
2023-12-08 11:23:52 -0300

    Add: Título del juego - Fuente de texto - Primeros estilos y colores

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import { Icon } from '@iconify/react';
 function App() {
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
-      <Icon icon={'fa6-solid:o'} width={'100'} />
-      <Icon icon={'fa6-solid:x'} width={'200'} />
+      <h1>
+        <b id='b-1'>Tic</b>-<b id='b-2'>Tac</b>-<b id='b-3'>Toe</b>
+      </h1>
+      {/* <Icon icon={'fa6-solid:o'} width={'100'} />
+      <Icon icon={'fa6-solid:x'} width={'200'} /> */}
     </div>
   );
 }
